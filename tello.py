c9a00e0bc7f482eb5965206055a2f65b7a596a77
Trevor Schupbach <trevorschupbach@gmail.com>
2020-07-26 16:10:54 -0400

    Finished gettting video
    Fixed video  and stuff

--- tello.py
+++ tello.py
@@ -1,5 +1,19 @@
 import Tello3
 
+#gets the frame in a safe way.
+#Program will temporarily hang if reciever is writing the image
+frame = None
+def getImage(instance):
+    global frame
+    instance.mutexLock.aquire()
+    if(instance.BFrame == None):
+        instance.mutexLock.release()
+        return -1
+    else:    
+        frame = instance.BFrame
+        instance.mutexLock.release()
+        return 1
+
 #instance of Tello3
 instance = Tello3.telloSDK()
 
---
+++ tello_dll/README.md
@@ -0,0 +1,3 @@
+#Installation (Windows x64 only)
+1. install vcredist_x64.exe (vs2013 compiler installation package)
+2. copy the remaining files in the folder to the .\Python\Python37\Lib\site-packages path. (except README.md)
