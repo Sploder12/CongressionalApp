396571bae4ab889fe11f495cd1e071c877342e01
Spatialflunky1 (Jake) <mjandrus06@gmail.com>
2020-07-30 00:25:56 -0400

    added script to get camera, still working out some kinks

--- live.py
+++ live.py
@@ -1,109 +1,43 @@
-import numpy as np
-import argparse
-import time
 import cv2
-import os
+import time
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-y", "--yolo", required=True, help="path to yolo DIR")
-ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probobility to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
-args = vars(ap.parse_args())
-video=cv2.VideoCapture(0, cv2.CAP_DSHOW)
-check, frame = video.read()
+try:
+    # Creates video object, 0 for external camera
+    video=cv2.VideoCapture(0)
 
-# load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
-LABELS = open(labelsPath).read().strip().split("\n")
+    # create frame object
+    check, frame = video.read()
+    print(check)
+    print(frame)
 
-# initialize a list of colors to represent each possible class label
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-# derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+    # shows the frame
+    cv2.imshow("Capturing", frame)
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    # add key to exit (milliseconds)
+    cv2.waitKey(0)
 
-# load our input image and grab its spatial dimensions
-image = cv2.imread(frame)
-(H, W) = image.shape[:2]
+    # shutdown the camera
+    video.release()
 
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    cv2.destroyAllWindows()
+except:
+    # Creates video object, 0 for external camera
+    video=cv2.VideoCapture(1)
 
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
-blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
+    # create frame object
+    check, frame = video.read()
+    print(check)
+    print(frame)
 
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-# initialize our lists of detected bounding boxes, confidences, and
-# class IDs, respectively
-boxes = []
-confidences = []
-classIDs = []
+    # shows the frame
+    cv2.imshow("Capturing", frame)
 
-# loop over each of the layer outputs
-for output in layerOutputs:
-	# loop over each of the detections
-	for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
-		scores = detection[5:]
-		classID = np.argmax(scores)
-		confidence = scores[classID]
-		
-        # filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
-		if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
-			box = detection[0:4] * np.array([W, H, W, H])
-			(centerX, centerY, width, height) = box.astype("int")
-			
-            # use the center (x, y)-coordinates to derive the top and
-			# and left corner of the bounding box
-			x = int(centerX - (width / 2))
-			y = int(centerY - (height / 2))
-            
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
-			boxes.append([x, y, int(width), int(height)])
-			confidences.append(float(confidence))
-			classIDs.append(classID)
+    # add key to exit (milliseconds)
+    cv2.waitKey(0)
 
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+    # shutdown the camera
+    video.release()
 
-# ensure at least one detection exists
-if len(idxs) > 0:
-	# loop over the indexes we are keeping
-	for i in idxs.flatten():
-		# extract the bounding box coordinates
-		(x, y) = (boxes[i][0], boxes[i][1])
-		(w, h) = (boxes[i][2], boxes[i][3])
-
-		# draw a bounding box rectangle and label on the image
-		color = [int(c) for c in COLORS[classIDs[i]]]
-		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    cv2.destroyAllWindows()
