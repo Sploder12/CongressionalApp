2fcc1642097cbabc27d57420cbfe5e4fc8f1f6ad
Trevor Schupbach <trevorschupbach@gmail.com>
2020-08-16 10:49:40 -0400

    polygoons
    you can fly in cool polygon shapes, still have to test if decimals work when sending messages

--- tello.py
+++ tello.py
@@ -9,11 +9,29 @@ def getImage(instance):
     instance.mutexLock.release()
     return frame
 
-#instance of Tello3
-instance = Tello3.telloSDK()
+# Flys in a polygon shape starting on the bottom leftmost pont
+#_in_ size : number 20 - 500 (centimeters)
+#_in_ sides : integer 3 - 360
+#_in_ instance : telloSDK instance
+#
+#_out_ status : bool -1?1
+def flyPolygon(size, sides, instance):
+    if(sides > 360 or sides < 3):
+        return -1
+    if(size > 500 or size < 20):
+        return -1
 
-img = getImage(instance)
+    rotation = 360/sides
+    if(sides != 4):
+        if(sides == 3): 
+            instance.sendMessage("cw 30")
+        else:
+            instance.sendMessage("ccw " + str(90-rotation))
 
-#Make sure you end it when done!
-instance.end()
-
+    for i in range(sides):
+        instance.sendMessage("forward " + str(size))
+        if i == sides:
+            instance.sendMessage("cw 90") #drone ends the same way it starts
+        else:
+            instance.sendMessage("cw " + str(rotation))
+    return 1
