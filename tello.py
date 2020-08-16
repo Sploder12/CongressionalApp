dc5c787bdc9f654b77fea7ecf93439ff0d8fcedd
Trevor Schupbach <trevorschupbach@gmail.com>
2020-08-16 11:52:22 -0400

    figure eight
    no way this works as is but its worth a shot

--- tello.py
+++ tello.py
@@ -1,5 +1,6 @@
 import Tello3
 import cv2
+import math
 
 #gets the frame in a safe way.
 #Program will temporarily hang if reciever is writing the image
@@ -34,4 +35,31 @@ def flyPolygon(size, sides, instance):
             instance.sendMessage("cw 90") #drone ends the same way it starts
         else:
             instance.sendMessage("cw " + str(rotation))
+    return 1
+
+#Flys in a figure 8 starting the the middle
+#_in_ size : number 50 - 500
+#_in_ instance : telloSDK instance
+#
+#_out_ status : bool -1?1
+def figure8(size, instance):
+    if(size < 50 or size > 500):
+        return -1
+
+    #we're heavily assuming that curve uses relative postion instead of some magical absolute position that makes life suck (documentation is unclear)
+    #also assuming that the drone's x/y/z axis are also relative to rotation
+    #5% chance this works as is
+    instance.sendMessage("cw 90")
+    x = lambda rad: math.sin(rad)*size
+    y = lambda rad: math.cos(rad)*size
+    pnt1 = (x(0.25*math.pi), y(1.25*math.pi) + size) 
+    pnt2 = (x(0.5*math.pi), y(0.5*math.pi) + size)
+    for i in range(4): #first circle
+        instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]))
+    
+    pnt1 = (x(0.25*math.pi), y(0.25*math.pi) - size) 
+    pnt2 = (x(0.5*math.pi), y(0.5*math.pi) - size)
+    for i in range(4): #second circle
+        instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]))
+
     return 1
