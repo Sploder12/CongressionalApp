ebf22a954d88bb4a690fb7a6b0a518d411823bc5
Will Oh <willoh2023@gmail.com>
2020-08-29 14:54:01 -0400

    uncommented stuff

--- GUI.py
+++ GUI.py
@@ -2,16 +2,15 @@ import sys
 import Tello3
 import tello
 import math
-# from getinfo import getinfo
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication, QProgressBar, QSlider, QLCDNumber)
+    QPushButton, QApplication, QProgressBar, QSlider, QLCDNumber, QLabel)
 from PyQt5.QtGui import QFont
 
 instance = Tello3.telloSDK()
 #where to end the instance???
 class guiForDrone(QWidget):
-
+    
     def forward(self):
         instance.sendMessage("forward 20")
     
@@ -46,108 +45,125 @@ class guiForDrone(QWidget):
     def land(self):
         instance.sendMessage("land")
 
-    def getspeed(self):
-        speed = instance.sendMessage("speed?")
-        return speed
-    def getbattery(self):
-        battery = instance.sendMessage("battery?")
-        return battery
-    def gettime(self):
-        time = instance.sendMessage("time?")
-        return time
-    def getheight(self):
-        height = instance.sendMessage("height?")
-        return height
-    def gettemp(self):
-        temp = instance.sendMessage("temp?")
-        return temp
-    def getaltitude(self):
-        altitude = instance.sendMessage("altitude?")
-        return altitude
-    def getbarometer(self):
-        barometer = instance.sendMessage("baro?")
-        return barometer
-    def getacceleration(self):
-        acceleration = instance.sendMessage("acceleration?")
-        return acceleration
-    def gettof(self):
-        tof = instance.sendMessage("tof?") # idk what tof is
-        return tof
-    def getwifi(self):
-        wifi = instance.sendMessage("wifi?")
-        return wifi
+    # def getspeed(self):
+    #     speed = instance.sendMessage("speed?")
+    #     return speed
 
-    def flyPolygon(self):
-        sides = 5
-        size = 150
-        if(sides > 360 or sides < 3):
-            return -1
-        if(size > 500 or size < 20):
-            return -1
+    # def getbattery(self):
+    #     battery = instance.sendMessage("battery?")
+    #     return battery
 
-        rotation = 360/sides
-        if(sides != 4):
-            if(sides == 3): 
-                instance.sendMessage("cw 30")
-            else:
-                instance.sendMessage("ccw " + str(90-rotation))
+    # def gettime(self):
+    #     time = instance.sendMessage("time?")
+    #     return time
 
-        for i in range(sides):
-            instance.sendMessage("forward " + str(size))
-            if i == sides:
-                instance.sendMessage("cw 90") #drone ends the same way it starts
-            else:
-                instance.sendMessage("cw " + str(rotation))
-        return 1
+    # def getheight(self):
+    #     height = instance.sendMessage("height?")
+    #     return height
 
-    #Flys in a figure 8 starting the the middle
-    #_in_ instance : telloSDK instance
-    #_in_ size : number 50 - 500
-    #_in_ speed : number 10-60 cm/s
-    #
-    #_out_ status : bool -1?1
-    def figure8(self):
-        size = 150
-        speed = 50
-        if(size < 50 or size > 500):
-            return -1
-        if(speed < 10 or speed > 60):
-            return -1
+    # def gettemp(self):
+    #     temp = instance.sendMessage("temp?")
+    #     return temp
 
-        #we're heavily assuming that curve uses relative postion instead of some magical absolute position that makes life suck (documentation is unclear)
-        #also assuming that the drone's x/y/z axis are also relative to rotation
-        #5% chance this works as is
-        instance.sendMessage("cw 90")
-        x = lambda rad: math.sin(rad)*size
-        y = lambda rad: math.cos(rad)*size
-        pnt1 = (x(0.25*math.pi), y(1.25*math.pi) + size) 
-        pnt2 = (x(0.5*math.pi), y(0.5*math.pi) + size)
-        for i in range(4): #first circle
-            instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]) + " " + str(speed))
+    # def getattitude(self):
+    #     attitude = instance.sendMessage("attitude?")
+    #     return attitude
 
-        pnt1 = (x(0.25*math.pi), y(0.25*math.pi) - size) 
-        pnt2 = (x(0.5*math.pi), y(0.5*math.pi) - size)
-        for i in range(4): #second circle
-            instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]) + " " + str(speed))
+    # def getbarometer(self):
+    #     barometer = instance.sendMessage("baro?")
+    #     return barometer
 
-        return 1
+    # def getacceleration(self):
+    #     acceleration = instance.sendMessage("acceleration?")
+    #     return acceleration
+
+    # def gettof(self):
+    #     tof = instance.sendMessage("tof?") # idk what tof is
+    #     return tof
+        
+    # def getwifi(self):
+    #     wifi = instance.sendMessage("wifi?")
+    #     return wifi
+
+    # def flyPolygon(self):
+    #     sides = 5
+    #     size = 150
+    #     if(sides > 360 or sides < 3):
+    #         return -1
+    #     if(size > 500 or size < 20):
+    #         return -1
+
+    #     rotation = 360/sides
+    #     if(sides != 4):
+    #         if(sides == 3): 
+    #             instance.sendMessage("cw 30")
+    #         else:
+    #             instance.sendMessage("ccw " + str(90-rotation))
+
+    #     for i in range(sides):
+    #         instance.sendMessage("forward " + str(size))
+    #         if i == sides:
+    #             instance.sendMessage("cw 90") #drone ends the same way it starts
+    #         else:
+    #             instance.sendMessage("cw " + str(rotation))
+    #     return 1
+
+    # #Flys in a figure 8 starting the the middle
+    # #_in_ instance : telloSDK instance
+    # #_in_ size : number 50 - 500
+    # #_in_ speed : number 10-60 cm/s
+    # #
+    # #_out_ status : bool -1?1
+    # def figure8(self):
+    #     size = 150
+    #     speed = 50
+    #     if(size < 50 or size > 500):
+    #         return -1
+    #     if(speed < 10 or speed > 60):
+    #         return -1
+
+    #     #we're heavily assuming that curve uses relative postion instead of some magical absolute position that makes life suck (documentation is unclear)
+    #     #also assuming that the drone's x/y/z axis are also relative to rotation
+    #     #5% chance this works as is
+    #     instance.sendMessage("cw 90")
+    #     x = lambda rad: math.sin(rad)*size
+    #     y = lambda rad: math.cos(rad)*size
+    #     pnt1 = (x(0.25*math.pi), y(1.25*math.pi) + size) 
+    #     pnt2 = (x(0.5*math.pi), y(0.5*math.pi) + size)
+    #     for i in range(4): #first circle
+    #         instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]) + " " + str(speed))
+
+    #     pnt1 = (x(0.25*math.pi), y(0.25*math.pi) - size) 
+    #     pnt2 = (x(0.5*math.pi), y(0.5*math.pi) - size)
+    #     for i in range(4): #second circle
+    #         instance.sendMessage("curve " + str(pnt1[0]) + " 0 " + str(pnt1[1]) + " " + str(pnt2[0]) + " 0 " + str(pnt2[1]) + " " + str(speed))
+
+    #     return 1
         
     def __init__(self):
         super().__init__()
+        global instance
 
         self.setWindowTitle("Tello Drone GUI")
         self.setFixedWidth(1280)
         self.setFixedHeight(720)
 
+        # self.speedLabel = QtWidgets.QLabel('Speed: ', self)
+        # self.batteryLabel = QtWidgets.QLabel('Battery: ', self)
+        # self.timeLabel = QtWidgets.QLabel('Time: ', self)
+        # self.heightLabel = QtWidgets.QLabel('Height: ', self)
+        # self.tempLabel = QtWidgets.QLabel('Temp: ', self)
+        # self.attitudeLabel = QtWidgets.QLabel('Attitude: ', self)
+        # self.barometerLabel = QtWidgets.QLabel('Barometer: ', self)
+        # self.accelerationLabel = QtWidgets.QLabel('Acceleration: ', self)
+        # self.tofLabel = QtWidgets.QLabel('TOF: ', self)
+        # self.wifiLabel = QtWidgets.QLabel('Wifi: ', self)
+
         self.initUI()
 
 
     def initUI(self):
-
-        self.updateLabels()
-        self.my_timer = QtCore.QTimer()
-        self.my_timer.timeout.connect(self.updateLabels())
-        self.my_timer.start(60000) #1 min intervall
+        global instance
 
         #forward
         forwardBtn = QPushButton('Forward', self)
@@ -219,39 +235,39 @@ class guiForDrone(QWidget):
         landBtn.setGeometry(QtCore.QRect(590, 525, 100, 50))
         landBtn.clicked[bool].connect(self.land)
 
-        speedLabel = QtWidgets.QLabel('Speed: ', self)
-        speedLabel.setGeometry(QtCore.QRect(900, 10, 75, 20))
+        # self.speedLabel = QtWidgets.QLabel('Speed: ', self)
+        # self.speedLabel.setGeometry(QtCore.QRect(900, 10, 75, 20))
 
-        batteryLabel = QtWidgets.QLabel('Battery: ', self)
-        batteryLabel.setGeometry(QtCore.QRect(900, 35, 75, 20))
+        # self.batteryLabel = QtWidgets.QLabel('Battery: ', self)
+        # self.batteryLabel.setGeometry(QtCore.QRect(900, 35, 75, 20))
 
-        timeLabel = QtWidgets.QLabel('Time: ', self)
-        timeLabel.setGeometry(QtCore.QRect(900, 60, 75, 20))
+        # self.timeLabel = QtWidgets.QLabel('Time: ', self)
+        # self.timeLabel.setGeometry(QtCore.QRect(900, 60, 75, 20))
 
-        heightLabel = QtWidgets.QLabel('Height: ', self)
-        heightLabel.setGeometry(QtCore.QRect(900, 85, 75, 20))
+        # self.heightLabel = QtWidgets.QLabel('Height: ', self)
+        # self.heightLabel.setGeometry(QtCore.QRect(900, 85, 75, 20))
 
-        tempLabel = QtWidgets.QLabel('Temp: ', self)
-        tempLabel.setGeometry(QtCore.QRect(900, 110, 75, 20))
+        # self.tempLabel = QtWidgets.QLabel('Temp: ', self)
+        # self.tempLabel.setGeometry(QtCore.QRect(900, 110, 75, 20))
 
-        altitudeLabel = QtWidgets.QLabel('Altitude: ', self)
-        altitudeLabel.setGeometry(QtCore.QRect(900, 135, 75, 20))
+        # self.attitudeLabel = QtWidgets.QLabel('Attitude: ', self)
+        # self.attitudeLabel.setGeometry(QtCore.QRect(900, 135, 75, 20))
 
-        barometerLabel = QtWidgets.QLabel('Barometer: ', self)
-        barometerLabel.setGeometry(QtCore.QRect(900, 160, 75, 20))
+        # self.barometerLabel = QtWidgets.QLabel('Barometer: ', self)
+        # self.barometerLabel.setGeometry(QtCore.QRect(900, 160, 75, 20))
 
-        accelerationLabel = QtWidgets.QLabel('Acceleration: ', self)
-        accelerationLabel.setGeometry(QtCore.QRect(900, 185, 81, 20))
+        # self.accelerationLabel = QtWidgets.QLabel('Acceleration: ', self)
+        # self.accelerationLabel.setGeometry(QtCore.QRect(900, 185, 81, 20))
 
-        tofLabel = QtWidgets.QLabel('TOF: ', self)
-        tofLabel.setGeometry(QtCore.QRect(900, 210, 75, 20))
+        # self.tofLabel = QtWidgets.QLabel('TOF: ', self)
+        # self.tofLabel.setGeometry(QtCore.QRect(900, 210, 75, 20))
 
-        wifiLabel = QtWidgets.QLabel('Wifi: ', self)
-        wifiLabel.setGeometry(QtCore.QRect(900, 235, 75, 20))
+        # self.wifiLabel = QtWidgets.QLabel('Wifi: ', self)
+        # self.wifiLabel.setGeometry(QtCore.QRect(900, 235, 75, 20))
 
-        flyPolygonBtn = QtWidgets.QPushButton('Fly Polygon', self)
-        flyPolygonBtn.setGeometry(QtCore.QRect(490, 660, 100, 30))
-        flyPolygonBtn.clicked[bool].connect(self.flyPolygon)
+        # flyPolygonBtn = QtWidgets.QPushButton('Fly Polygon', self)
+        # flyPolygonBtn.setGeometry(QtCore.QRect(490, 660, 100, 30))
+        # flyPolygonBtn.clicked[bool].connect(self.flyPolygon)
 
         # sidesPolygon = QtWidgets.QSpinBox(self)
         # sidesPolygon.setGeometry(QtCore.QRect(490, 630, 42, 22))
@@ -262,12 +278,12 @@ class guiForDrone(QWidget):
         # sizePolygon = QtWidgets.QSpinBox(self)
         # sizePolygon.setGeometry(QtCore.QRect(560, 630, 42, 22))
 
-        # label_2 = QtWidgets.QLabel('Sides')
+        # label_2 = QtWidgets.QLabel('Sides', self)
         # label_2.setGeometry(QtCore.QRect(560, 610, 55, 16))
         
-        figure8Btn = QtWidgets.QPushButton('Figure 8', self)
-        figure8Btn.setGeometry(QtCore.QRect(680, 660, 100, 30))
-        figure8Btn.clicked[bool].connect(self.figure8)
+        # figure8Btn = QtWidgets.QPushButton('Figure 8', self)
+        # figure8Btn.setGeometry(QtCore.QRect(680, 660, 100, 30))
+        # figure8Btn.clicked[bool].connect(self.figure8)
 
         # label_3 = QtWidgets.QLabel('Size', self)
         # label_3.setGeometry(QtCore.QRect(670, 610, 55, 16))
@@ -281,31 +297,36 @@ class guiForDrone(QWidget):
         # sides8 = QtWidgets.QSpinBox(self)
         # sides8.setGeometry(QtCore.QRect(670, 630, 42, 22))
 
+        # self.updateLabels()
+        # self.my_timer = QtCore.QTimer()
+        # self.my_timer.timeout.connect(self.updateLabels)
+        # self.my_timer.start(5000) #5sec interval 
+
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
-    def updateLabels(self):
-        speed = str(getspeed())
-        battery = str(getbattery())
-        time = str(gettime())
-        height = str(getheight())
-        temp = str(gettemp())
-        altitude = str(getaltitude())
-        barometer = str(getbarometer())
-        acceleration = str(getacceleration())
-        tof = str(gettof())
-        wifi = str(getwifi())
-        speedLabel.setText(speed)
-        batteryLabel.setText(battery)
-        timeLabel.setText(time)
-        heightLabel.setText(height)
-        tempLabel.setText(temp)
-        altitudeLabel.setText(altitude)
-        barometerLabel.setText(barometer)
-        accelerationLabel.setText(acceleration)
-        tofLabel.setText(tof)
-        wifiLabel.setText(wifi)
-        self.update()
+    # def updateLabels(self):
+    #     speed = "Speed: " + str(self.getspeed())
+    #     battery = "Battery: " + str(self.getbattery())
+    #     time = str(self.gettime())
+    #     height = str(self.getheight())
+    #     temp = str(self.gettemp())
+    #     attitude = str(self.getattitude())
+    #     barometer = str(self.getbarometer())
+    #     acceleration = str(self.getacceleration())
+    #     tof = str(self.gettof())
+    #     wifi = str(self.getwifi())
+    #     self.speedLabel.setText(speed)
+    #     self.batteryLabel.setText(battery)
+    #     self.timeLabel.setText(time)
+    #     self.heightLabel.setText(height)
+    #     self.tempLabel.setText(temp)
+    #     self.attitudeLabel.setText(attitude)
+    #     self.barometerLabel.setText(barometer)
+    #     self.accelerationLabel.setText(acceleration)
+    #     self.tofLabel.setText(tof)
+    #     self.wifiLabel.setText(wifi)
+    #     self.update()
 
 def main():
 
--- Tello3.py
+++ Tello3.py
@@ -15,7 +15,7 @@ class telloSDK:
         # Create a UDP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.tello_address = (constant.TELLO_IP, port) #change TELLO_IP to LOCAL_IP if testing without drone
+        self.tello_address = (constant.LOCAL_IP, port) #change TELLO_IP to LOCAL_IP if testing without drone
 
         self.sock.bind((constant.LOCAL_IP, port))
 
@@ -40,8 +40,8 @@ class telloSDK:
         self.sendMessage("streamon") #starts video stream
 
         self.ret = False
-        self.telloVideo = cv2.VideoCapture("udp://@" + constant.TELLO_IP + ":" + str(self.local_video_port))
-        #self.telloVideo = cv2.VideoCapture("test.mp4") #used for testing when Tello not present
+        #self.telloVideo = cv2.VideoCapture("udp://@" + constant.TELLO_IP + ":" + str(self.local_video_port))
+        self.telloVideo = cv2.VideoCapture("test.mp4") #used for testing when Tello not present
         self.scale = 3
 
         #create video thread
